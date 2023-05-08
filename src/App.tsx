3bfc65eee87b6569f246527ae24b2ac8915eeffb
unknown <thiago.barreto.oliveira22@gmail.com>
2023-05-08 01:13:32 -0300

    Learning RRD

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,35 @@
-import { useState } from "react";
-import { Tweet } from "./components/Tweet"
-import "./App.css"
-import { AppRoutes } from "./Routes";
+import Titulo from './Titulo'
+import About from './About'
+import State from './State';
+import Navbar from './NavBar';
+import Home from './Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  // const [tweets, setTweets] = useState<string[]>([
-  //   "Tweet 1",
-  //   "Tweet 2",
-  //   "Tweet 3",
-  //   "Tweet 4",
-  //   "Tweet 5",
-  // ])
+function App(){
 
-  // function createTweet() {
-  //   setTweets([...tweets, "Tweet 6"])
-  // }
+  const nome = "Thiago"
+  const paragrafo = "Parágrafo de teste"
+  const cor = "red"
+  const soma = 20;
+  const name = 'John Doe';
+  const urlimg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png';
+
 
   return (
-    <AppRoutes />
-    // <div>
-    //   {tweets.map(tweet => {
-    //     return <Tweet text={tweet}/>
-    //   })}
-
-    //   <button
-    //    onClick={createTweet}
-    //    style={{
-    //     backgroundColor: "#8257e6",
-    //     border: 0,
-    //     padding: "6px 12px",
-    //     color: "#FFF",
-    //     cursor: "pointer"
-    //    }}
-    //    >
-    //     Adicionar tweet
-    //    </button>
-    // </div>
-  );
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path='/' element={<Home nome={nome}/>}/>
+            <Route path="/input" element={<State color="yellow" texto="Texto" />}/>
+            <Route path="/titulo" element={<Titulo nome={nome} paragrafo={paragrafo} cor={cor}/>}/>
+            <Route path="/about" element={<About soma={soma} name={name} urlimg={urlimg} color={"purple"} />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
